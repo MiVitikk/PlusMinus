c18c25410ef6f58440cd068991ddf5a09eca5c6f
Hilla Nokela <mikko.vitikka@myy.haaga-helia.fi>
2025-01-18 09:35:08 +0200

    Exercise complete

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [number1, setNumber1] = useState('')
+  const [number2, setNumber2] = useState('')
+  const [answer, setAnswer] = useState('')
+
+  const clearInputs = () => {
+    setNumber1()
+    setNumber2()
+  }
+
+  const plusPressed = () => {
+    
+    setAnswer(parseFloat(number1) + parseFloat(number2))
+    clearInputs()
+  }
+
+  const minusPressed = () => {
+    setAnswer(parseFloat(number1) - parseFloat(number2))
+    clearInputs()
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {answer}</Text>
       <StatusBar style="auto" />
+      <View style={{ flexDirection: 'column'}}>
+        <TextInput
+          style={styles.input}
+          onChangeText={number1 => (setNumber1(number1))}
+          value={number1}
+          keyboardType="numeric" />
+        <TextInput
+          style={styles.input}
+          onChangeText={number2 => (setNumber2(number2))}
+          value={number2}
+          keyboardType="numeric" />
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', gap: '20' }}>
+        <Button onPress={plusPressed} title="+" />
+        <Button onPress={minusPressed} title="-" />
+      </View>
     </View>
   );
 }
@@ -16,5 +53,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
+    
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 100
   },
 });
